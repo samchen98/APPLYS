0b59d60cdb6476b28703d2441a81988879f4f135
Sunnie Kwak <sunnie@live.unc.edu>
2019-12-07 20:53:02 -0500

    survey functionality

--- APPLYS/backend/routes/questions.js
+++ APPLYS/backend/routes/questions.js
@@ -33,20 +33,15 @@ router.route('/postquestion').post((req, res) => {
 });
 
 router.route('/getquestion').get((req, res) => {
-    console.log("getQ");
-  
     const { body } = req;
     let {
       id,
     } = body;
-  
     Question.find({
-      id: 1
+      id: req.query.id
     }, (err, questions) => {
-      
       const question = questions[0];
       return res.send({
-          
           message: questions[0]
         });
     
@@ -54,80 +49,12 @@ router.route('/getquestion').get((req, res) => {
 
 });
 
-
-
-router.route('/add').post((req, res) => {
-  console.log("is this hit?");
-
-  const { body } = req;
-  let {
-    fname,
-    lname,
-    email,
-    password,
-    userType
-  } = body;
-  
-  console.log(email);
-  if (!email) {
-    return res.send({
-      success: false,
-      message: 'Error: Email cannot be blank.'
-    });
-  }
-  if (!password) {
-    return res.send({
-      success: false,
-      message: 'Error: Password cannot be blank.'
-    });
-  }
-
-  email = email.toLowerCase();
-  email = email.trim();
-
-  // Steps:
-  // 1. Verify email doesn't exist
-  // 2. Save
-  User.find({
-    email: email
-  }, (err, previousUsers) => {
-    if (err) {
-      return res.send({
-        success: false,
-        message: 'Error: Server error'
-      });
-    } else if (previousUsers.length > 0) {
-      return res.send({
-        success: false,
-        message: 'Error: Account already exist.'
-      });
-    }
-
-    // Save the new user
-    const newUser = new User();
-    newUser.fname = fname;
-    newUser.lname = lname;
-    newUser.userType = userType;
-    newUser.email = email;
-    newUser.password = newUser.generateHash(password);
-    newUser.save((err, user) => {
-      if (err) {
-        return res.send({
-          success: false,
-          message: 'Error: Server error'
-        });
-      }
-      return res.send({
-        success: true,
-        message: 'Signed up'
-      });
-    });
-  });
-
-
-});
-
-
+// router.route('/getSize').get((req, res) => {
+//   db.collection.count({}, function(error, numOfDocs) {
+//     console.log('I have '+numOfDocs+' documents in my collection');
+//     // ..
+//   });
+// });
 
 
 module.exports = router;
