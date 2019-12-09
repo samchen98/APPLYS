a2e3272719037becb77ed86b9b9f46c66d89665b
chen16s <chen16s@live.unc.edu>
2019-12-08 22:34:36 -0500

    updated server

--- APPLYS/config.js
+++ APPLYS/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //serversite: 'https://powerful-savannah-08407.herokuapp.com',
-    serversite: 'http://localhost:5000',
+    serversite: 'https://infinite-mountain-45827.herokuapp.com',
+    // serversite: 'http://localhost:5000',
     dbLink: "mongodb+srv://sam:samchen1131@cluster0-xwslr.mongodb.net/test?retryWrites=true&w=majority"
     //dbLink: "mongodb://127.0.0.1:27017/database"
     
